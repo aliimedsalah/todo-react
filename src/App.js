20eb5a5ae0c924f43bd4c46a0a4461a1f628c690
aliimedsalah <61020141+aliimedsalah@users.noreply.github.com>
2020-03-29 16:00:32 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Todo from './Components/todo'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+        <Todo/>
+    </div>
+  )
+}
+
+
+
+
+
